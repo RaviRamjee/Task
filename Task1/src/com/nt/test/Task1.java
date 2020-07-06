f87fbaf1e5f4ffb83786a55649fa476df6f329a9
RaviRamjee <ravi.ramjee@anywhere.co>
2020-07-06 13:52:19 +0530

    1st task

---
+++ Task1/src/com/nt/test/Task1.java
@@ -0,0 +1,11 @@
+package com.nt.test;
+
+public class Task1 {
+
+	public String task()
+	{
+		String s="Ravi";
+		
+		return s;
+	}
+}
---
+++ Task1/src/com/nt/test/Task1Test.java
@@ -0,0 +1,13 @@
+package com.nt.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Task1Test extends Task1 {
+
+	@Test
+	void test() {
+		assertEquals("Ravi",task());
+	}
+}
